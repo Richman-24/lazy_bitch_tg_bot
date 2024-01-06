2af90276d6520f3e5fc4bc044dd8c0d2d258d140
Maksim <dreev24@yandex.ru>
2024-01-06 18:29:27 +0300

    Основной функционал модуля WINE готов, добавлены интсрументы работы с БД (много багов)

---
+++ database_commands.py
@@ -0,0 +1,68 @@
+from PIL import Image, ImageDraw, ImageFont
+import sqlite3
+import io
+
+def draw_image(path: str, mark: int):
+    """Функция берёт картинку и оценку, рисует оценку на картинке и возвращает 
+    в BLOB формате для записи в БД"""
+    with Image.open(path) as im:
+        draw_text = ImageDraw.Draw(im)
+        font = ImageFont.truetype("C:\Proj\LB_BOT\spaceranger-rus.otf", size=200)
+        fill = '#fa0000' if mark < 5 else '#00e600' #определяет цвет заполнителя, в зависимости от оценки 
+        
+        draw_text.text( #рисуем оценку на картинке
+            (50, 50),
+            str(mark)+'\\10',
+            font=font,
+            fill = fill)
+    
+    
+    data = io.BytesIO() #Используем io.BytesIO() как временное хранилище картинки, чтобы не сохранять на диск
+    im.save(data, format='jpeg') #Сохраняем в локальном хранилище, в формате jpeg
+    return data.getvalue() #Получаем байтовый поток изображения
+
+
+def insert_blob_to_db(type_wine, title, mark, description, path_photo):
+    """Вставляет данные в SQL"""
+    try:
+        connection = sqlite3.connect('alcho_db.db')
+        cursor = connection.cursor()
+        print("Подключен к SQLite")
+
+        insert_query = """INSERT INTO wine_db (type_wine, title, mark, description, photo)
+                          VALUES(?, ?, ?, ?, ?)"""
+
+        photo = draw_image(path_photo, mark) #
+        # Преобразование данных в формат кортежа
+        data_tuple = (type_wine, title, mark, description, photo)
+        cursor.execute(insert_query, data_tuple)
+        connection.commit()
+        print("Изображение и файл успешно вставлены как BLOB в таблицу")
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка при работе с SQLite", error)
+    finally:
+        if connection:
+            connection.close()
+            print("Соединение с SQLite закрыто")
+
+def ask_data_base(wine: str, flag: str):
+    """Готовит список из БЛОБ фото по типу вина и оценке (переданных в функцию)"""
+    try:
+        connection = sqlite3.connect('alcho_db.db')
+        cursor = connection.cursor()
+        print("Подключен к alcho_db")
+        
+        data = list(cursor.execute('SELECT photo FROM wine_db WHERE type_wine == "{}" AND mark {} 5 ORDER BY mark'.format(wine, flag)))
+        print("Выполнен запрос")
+
+        return data
+
+
+    except sqlite3.Error as error:
+        print("Ошибка при работе с SQLite", error)
+    finally:
+        if connection:
+            connection.close()
+            print("Соединение с SQLite закрыто")
--- handlers/wine.py
+++ handlers/wine.py
@@ -1,15 +1,14 @@
 from aiogram import Router, F, types
 from aiogram.filters import Command
+from aiogram.types import FSInputFile, BufferedInputFile
+from aiogram.utils.media_group import MediaGroupBuilder
+
 from keyboards.wine_keyboards import tastes_keyboard, type_of_wine
+from database_commands import ask_data_base, image_from_bytes
 
-
-flag = -1
+flag = '>'
 router = Router()
 
-mark_tuple = ('<', '>')
-bd_querry = """SELECT photo 
-FROM ALCO_BD
-WHERE type = {} AND mark {} 5""".format(F.data, mark_tuple[flag])
 
 #кнопка /wine - запускает инлайн клавиатуру !Вкусное-невкусное
 @router.message(Command("wine"))
@@ -21,26 +20,17 @@ async def cmd_alcho_lib(message: types.Message):
     )
 
 
-#Кнопка на инлайн ВКУСНОЕ, убирает старую инлайн и запускает новую с выбором типа вина
+#Кнопка на инлайн ВКУСНОЕ|Невкусное, убирает старую инлайн и запускает новую с выбором типа вина
 @router.callback_query(F.data == "taste")
+@router.callback_query(F.data == "not_taste")
 async def cmd_taste(callback: types.CallbackQuery):
     global flag
-    flag = 1
+    flag = (">" if callback.data == 'taste' else "<")
+    #print(callback.data)
     await callback.message.answer(
-        "Инлайн с типом вина, flag = 1", 
+        "Какое вино вам показать?", 
         reply_markup=type_of_wine().as_markup())
-    #await callback.answer()
-
-
-#Кнопка на инлайн НЕВКУСНОЕ, убирает старую инлайн и запускает новую с выбором типа вина
-@router.callback_query(F.data == "not_taste")
-async def cmd_not_taste(callback: types.CallbackQuery):
-    global flag
-    flag = 0
-    await callback.message.answer(
-        "Инлайн с типом вина, flag = 0",
-        reply_markup=type_of_wine().as_markup())
-    #await callback.answer()
+    await callback.answer()
 
 #кнопка посылает тип вина, который потом вставляется в запрос к БД
 @router.callback_query(F.data == 'red')
@@ -48,10 +38,28 @@ async def cmd_not_taste(callback: types.CallbackQuery):
 @router.callback_query(F.data == 'pink')
 @router.callback_query(F.data == 'champaign')
 @router.callback_query(F.data == 'fruit')
-async def cmd_type_of_wine(callback: types.CallbackQuery):
-    await callback.message.answer(
-        "ЗАПРОС К БД С ТИПОМ ВИНА," + str(callback.data) + "И ФЛАГОМ flag" + str(flag))
+async def cmd_album(callback: types.CallbackQuery):
+    album_builder = MediaGroupBuilder(
+        caption="Общая подпись для будущего альбома")
+    try:
+        for i in ask_data_base(callback.data, flag):
+            album_builder.add(
+                type="photo",
+                media=BufferedInputFile(i[0], "image.jpeg"))
+            
+        await callback.message.answer_media_group(
+            media=album_builder.build())
+    except Exception as err:
+        print("Ошибка в функции cmd_album",err)
+
+'''async def cmd_type_of_wine(callback: types.CallbackQuery):
+    bd_querry = """SELECT photo 
+    FROM ALCO_BD
+    WHERE type = {} AND mark {} 5""".format(callback.data, flag)
+    #await callback.message.answer(bd_querry)
+    await callback.message.answer
     await callback.answer()
+'''   
 
 """    image_from_pc = types.FSInputFile("C:\\Proj\\LB_BOT\\alcho-BD\\red-d-1.jpg")
 await callback.message.answer_photo(
@@ -59,3 +67,6 @@ await callback.message.answer_photo(
         caption="Красное, сухое\n !!! ТУТ БУДЕТ АЛЬБОМ ВКУСНЫХ ВИН !!!\n 19 Crimes Cabernet Sauvignon\n\
             Сухое, плотное, мягкое, кислотность невысокая, послевкусие невыраженное\n\
                 Оценка: 4/5, FLAG = 1", reply_markup=types.ReplyKeyboardRemove())"""
+
+
+
